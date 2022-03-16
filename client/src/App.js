6901eeb9507a7f134f33cd10811dda1d6cda4d34
alexisn84 <elamrani.alexis@gmail.com>
2022-03-15 20:48:12 -0400

    updated folder mapout

--- client/src/App.js
+++ client/src/App.js
@@ -9,18 +9,10 @@ import {
 import { setContext } from '@apollo/client/link/context';
 
 //import componenets
-import Header from './components/Header';
-import Footer from './components/Footer';
+
 
 //import pages
-import Home from './pages/Home';
-import Login from './pages/Login';
-import MaidDashboard from './pages/MaidDashboard';
-import NoMatch from './pages/NoMatch';
-import SingleReview from './pages/SingleReview';
-import UserDashboard from './pages/UserDashboard';
-import Signup from './pages/Signup';
-import NavBar from './components/Navbar';
+
 
 //setup graphql and auth
 const httpLink = createHttpLink({
@@ -47,18 +39,7 @@ function App() {
         <ApolloProvider client={client}>
           <Router>  
               <div>
-              <NavBar />
-              <div>
-                  <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/login" component={Login} />
-                    <Route exact path="/signup" component={Signup} />
-                    <Route exact path="/dashboard/:username?" component={UserDashboard} />
-                    <Route exact path="/review/:id" component={SingleReview} />
-                    <Route exact path="/employee/:id" component={MaidDashboard} />
-                  </Switch>
-              </div>
-              <Footer />
+              
               </div>
             </Router>
         </ApolloProvider>
