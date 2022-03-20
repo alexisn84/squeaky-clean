6324e13892111e01847541276654403ad4061a9c
alexisn84 <elamrani.alexis@gmail.com>
2022-03-20 17:19:38 -0400

    component.ReviewList

--- client/src/components/ReviewList/index.js
+++ client/src/components/ReviewList/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const ReviewList = () => {
+const ReviewList = ({ reviews }) => {
+    if (!reviews.length) {
+        return <h3>No Reviews Yet</h3>
+    }
 
     return (
-        <div></div>
+        <div>
+            <h3>Reviews</h3>
+            {reviews &&
+                reviews.map((review) => (
+                    <div>
+                        <div>
+                            <Link to={`/review/${review._id}`}>
+                                <p>{review.reviewText}</p>
+                                </Link>
+                        </div>
+                    </div>
+                ))}
+        </div>
     );
 };
 
