8feb02d5fed05bc1012dd2ca043b3eed67bb302d
Ivy Do <ivy.do2011@gmail.com>
2022-03-24 14:14:12 -0400

    footer component complete

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,42 +1,62 @@
 import React from 'react'
+import './Footer.css'
 
 function Footer() {
     return (
         <div className='footer'>
             <div className='content has-text-centered'>
-                <div class="columns">
+                <div class="columns is-mobile is-centered">
                     <div class="column">
-                        <h4>
+                        <h4 className='title has-text-centered'>
                             Sparkling Spaces
                         </h4>
-                        <ul className='list-unstyled'>
-                            <li>407-SPA-RKLE</li>
-                            <li>123 Main St.</li>
-                            <li>Orlando, FL</li>
-                        </ul>
+                        <div className='list'>
+                            <ul>
+                                <div className='list-item'>
+                                    <li>407-SPA-RKLE</li>
+                                    <li>123 Main St.</li>
+                                    <li>Orlando, FL 32832</li>
+                                </div>
+                            </ul>
+                        </div>
                     </div>
                     <div class="column">
-                        <h4>MERN Project by</h4>
-                        <ul className='list-unstyled'>
-                            <li>Alexis El Armani</li>
-                            <li>Mark Dale</li>
-                            <li>Ivy Do</li>
-                            <li>Patricia Manneci</li>
-                            <li>Shawna Boucher</li>
-                        </ul>
+                        <h4 className='title has-text-centered'>MERN Project by</h4>
+                        <div className='list'>
+                            <ul>
+                                <div className='list-item'>
+                                    <li>Alexis El Armani</li>
+                                    <li>Mark Dale</li>
+                                    <li>Ivy Do</li>
+                                    <li>Patricia Manneci</li>
+                                    <li>Shawna Boucher</li>
+                                </div>
+                            </ul>
+                        </div>
                     </div>
                     <div class="column">
-                        <h4>Corporate</h4>
-                        <ul className='list-unstyled'>
-                            <a href="/maidlogin">                        
-                            <li>Employee Login</li>
-                            </a>
-                            {/* Filler List Items */}
-                            <li>Contact Us</li>
-                            <li>Customer Support</li>
-                            <li>Company Policies</li>
-                        </ul>
+                        <h4 className='title has-text-centered'>Corporate</h4>
+                        <div className='list'>
+                            <ul>
+                                <div className='list-item'>
+                                    <a href="/maidlogin">
+                                        <li>Employee Login</li>
+                                    </a>
+                                    {/* Filler List Items */}
+                                    <li>Customer Support</li>
+                                    <li>Company Policies</li>
+                                    <li>Contact Us</li>
+
+                                </div>
+                            </ul>
+                        </div>
                     </div>
+                </div>
+                <hr/>
+                <div className='column has-text-is-centered'>
+                    <p>
+                        &copy; {new Date().getFullYear()} SPARKLING SPACES | All Rights Reserved | Terms of Service 
+                    </p>
                 </div>
             </div>
         </div>
