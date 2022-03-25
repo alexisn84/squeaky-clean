e334a6833ead24a2708f3b5f060853346f3c3a81
sboucher <sboucher2011@gmail.com>
2022-03-25 11:11:10 -0400

    initial review form setup

--- client/src/components/ReviewForm/index.js
+++ client/src/components/ReviewForm/index.js
@@ -2,13 +2,32 @@ import React, { useState } from 'react';
 
 import { useMutation } from '@apollo/client';
 
-
 const ReviewForm = () => {
+    // update state based on form input changes
+    const handleChange = (event) => {
+
+    };
+
+    // submit form
+    const handleFormSubmit = async (event) => {
+
+    };
 
     return (
-        <div>
-            <form></form>
-        </div>
+        <form
+            className="flex-row justify-center justify-space-between-md align-stretch"
+            onSubmit={handleFormSubmit}
+        >
+            <textarea
+                placeholder="Here's a new thought..."
+                value={'thoughtText - remove quotes'}
+                className="form-input col-12 col-md-9"
+                onChange={handleChange}
+            ></textarea>
+            <button className="btn col-12 col-md-3" type="submit">
+                Submit
+            </button>
+        </form>
     );
 };
 
